b585398d141c6da14d43cb77d276711830be2d22
Defaulty731 <58785647+Defaulty731@users.noreply.github.com>
2026-06-29 00:34:23 -0700

    Delete data/characters/picoweird.hx

--- data/characters/picoweird.hx
+++
@@ -1,5 +0,0 @@
-function onCreatePost() {
-    if (ClientPrefs.bfSkin == 'BeastFriend' && game.boyfriend.curCharacter == 'picoweird') { // Check for song and skin player is using
-		triggerEventNote('Change Character', '0', 'PicoFriend');
-	}
-}
