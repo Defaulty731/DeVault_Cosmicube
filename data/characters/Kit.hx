ca1016d1f58ec7d5af4e41ffcd8156b87ab7359b
Defaulty731 <58785647+Defaulty731@users.noreply.github.com>
2026-07-13 22:08:08 -0700

    Delete data/characters/Kit.hx

--- data/characters/Kit.hx
+++
@@ -1,8 +0,0 @@
-function onCreatePost(){
-    if (curSong == 'roomcode' && ClientPrefs.bfSkin == 'Kit' && game.boyfriend.curCharacter == 'picolobby') { // Check for song and skin player is using
-        changeCharacter(0, 'Kaboodle');
-            if (ClientPrefs.pet == "Kaboodle") { // Check if player has kaboodle pet, if it does, it just makes the kaboodle pet disapper
-            pet.alpha = 0.001;
-            }
-	}
-}
